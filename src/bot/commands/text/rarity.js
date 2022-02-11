247be2dcb14f970d6460549ea7fe2895be026263
Az-r-ow <antoine.azar123@hotmail.com>
2022-02-11 17:48:43 +0100

    Fixed some bugs with the rarity command Added deleteDuplicates Script to remove duplicates of the same collection

--- src/bot/commands/text/rarity.js
+++ src/bot/commands/text/rarity.js
@@ -12,82 +12,96 @@ import {
 
 export const command = {
   name: 'rarity',
-  usage: `${prefix} rarity collection <collection_id>\n\n${prefix} rarity token <token_address>\n\n${prefix} rarity rank <collection_name> <rank>`,
+  usage: `\`${prefix} rarity collection <collection_id>\`\n\n\`${prefix} rarity token <token_address>\`\n\n\`${prefix} rarity rank <collection_name> <rank>\``,
   description: 'Check the rarity of a given NFT',
   async execute(interaction, args, client){
+
     if(args.length <= 1)throw new MissingArgsError(this.usage);
 
-    let nftData = args[0].toLowerCase() === "token" ? await Nft.findOne({hash: args[1]}).exec()
-                                                    : args[0].toLowerCase() === "rank" && args.join(" ").match(/\s\d+/g).length
-                                                    ? await Nft.findOne({rank: args.filter(arg => !isNaN(arg))[0], collection_name: args.slice(1).filter(arg => isNaN(arg)).join(" ").toUpperCase()}).exec()
-                                                     : await Nft.findOne({collection_name: args.slice(1).join(" ").toUpperCase()}).exec()
+    switch (args[0].toLowerCase()) {
+      case "token":
+        const nftData = await Nft.findOne({hash: args[1]}).exec()
+        if(!nftData)throw {
+          name: "Nothing was found !",
+          message: "This nft is not listed in our DB."
+        }
+        break;
+      case "rank":
+        const nftData1 = await Nft.findOne({rank: args.filter(arg => !isNaN(arg))[0], collection_name: args.slice(1).filter(arg => isNaN(arg)).join(" ").toUpperCase()}).exec();
+        if(!nftData1) await similarCollections(args);
+        await displayRarity(nftData1);
+        break;
+      case "collection":
+        const nftData2 = await Nft.findOne({collection_name: args.slice(1).filter(arg => isNaN(arg)).join(" ").toUpperCase(), collection_number: args.filter(arg => !isNaN(arg))[0]}).exec();
+        if(!nftData2) await similarCollections(args);
+        await displayRarity(nftData2);
+        break;
+      default:
+      throw new MissingArgsError(this.usage);
+    }
 
-
-    if (!nftData && (args[0].toLowerCase() === "collection" || args[0].toLowerCase() === "rank")){
+    // Look for collections with similar names
+    async function similarCollections(args){
       // Finding similar collection names
       const regex =  new RegExp(`${args.slice(1).filter(arg => isNaN(arg)).join(" ")}`);
-      console.log(args.slice(1).filter(arg => isNaN(arg)).join(" "))
       const similar = await Collection.find({name: {$regex: regex, $options: 'i'}}).exec();
-      if(!similar)throw {
+      if(!similar.length)throw {
         name: "Nothing was found !",
         message: "This collection might not be listed."
-      }; 
+      };
       throw {
         name: "Nothing was found !",
         message: `You might be searching for : \`\`\`${similar.map(i => i.name).join('\n')}\`\`\``
       }
-    }else if (!nftData){
-      throw {
-        name: "Nothing was found !",
-        message: "This collection might not be listed."
+    }
+
+    async function displayRarity(nftData){
+      const collectionData = await Collection.findOne({name: nftData.collection_name}).exec();
+      // Incrementing the view count of the collection
+      // collectionData = collectionData.rarityCheck + 1;
+      // await collectionData.save();
+
+      const footer = `${collectionData.blockchain} • ${collectionData.algo}`;
+
+      const displayRarity = {
+        embeds: [new DisplayRarity(nftData.name,nftData.rank,collectionData.numPieces.toString(),nftData.image,nftData.tier, footer)],
+        components: [new MessageActionRow()
+        .addComponents(
+          new MessageButton()
+          .setCustomId('attributes')
+          .setLabel('Attributes')
+          .setStyle('PRIMARY'),
+        ),]
+      };
+
+      const displayAttributes = {
+        embeds: [new DisplayAttributes(nftData.name, nftData.attributes, nftData.tier, footer)],
+        components: [new MessageActionRow()
+        .addComponents(
+          new MessageButton()
+          .setCustomId('rarity')
+          .setLabel('Rarity')
+          .setStyle('PRIMARY'),
+        ),]
       }
-    }
 
-    const collectionData = await Collection.findOne({name: nftData.collection_name}).exec();
-    // Incrementing the view count of the collection
-    // collectionData = collectionData.rarityCheck + 1;
-    // await collectionData.save();
+      interaction.reply(displayRarity)
+      .then(async message => {
+        const filter = c_interaction => c_interaction.isButton() && c_interaction.user.id === interaction.member.id;
 
-    const footer = `${collectionData.blockchain} • ${collectionData.algo}`;
+        const collector = message.createMessageComponentCollector({filter, idle: 15000});
 
-    const displayRarity = {
-      embeds: [new DisplayRarity(nftData.name,nftData.rank,collectionData.numPieces.toString(),nftData.image,nftData.tier, footer)],
-      components: [new MessageActionRow()
-      .addComponents(
-        new MessageButton()
-        .setCustomId('attributes')
-        .setLabel('Attributes')
-        .setStyle('PRIMARY'),
-      ),]
-    };
+        collector.on('collect', i => {
+          // Toggeling between attributes and rarity
+          i.customId === 'attributes' ? message.edit(displayAttributes) : message.edit(displayRarity);
+          return i.deferUpdate();
+        });
 
-    const displayAttributes = {
-      embeds: [new DisplayAttributes(nftData.name, nftData.attributes, nftData.tier, footer)],
-      components: [new MessageActionRow()
-      .addComponents(
-        new MessageButton()
-        .setCustomId('rarity')
-        .setLabel('Rarity')
-        .setStyle('PRIMARY'),
-      ),]
-    }
-
-    interaction.reply(displayRarity)
-    .then(async message => {
-      const filter = c_interaction => c_interaction.isButton() && c_interaction.user.id === interaction.member.id;
-
-      const collector = message.createMessageComponentCollector({filter, idle: 15000});
-
-      collector.on('collect', i => {
-        i.customId === 'attributes' ? message.edit(displayAttributes) : message.edit(displayRarity);
-        return i.deferUpdate();
-      });
-
-      collector.on('end', () => {
-        message.components[0].components.forEach(button => button.disabled = true);
-        message.edit({components: message.components}).catch(e => console.log(e))
+        collector.on('end', () => {
+          message.components[0].components.forEach(button => button.disabled = true);
+          message.edit({components: message.components}).catch(e => console.log(e))
+        })
       })
-    })
-
+    }
   }
 }
